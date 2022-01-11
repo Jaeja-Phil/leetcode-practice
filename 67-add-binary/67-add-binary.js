b8109bdd2299f7b3571ba80b06e9ec00258b2a2b
Wonjun (Phillip) Choi <jaeja.phil@gmail.com>
2022-01-11 20:38:02 +0900

    Time: 110 ms (27.64%), Space: 41.2 MB (29.70%) - LeetHub

--- 67-add-binary/67-add-binary.js
+++ 67-add-binary/67-add-binary.js
@@ -1,35 +1,13 @@
 var addBinary = function(a, b) {
-    if (a.length > b.length) {
-        [a, b] = [b, a];
+    var result = "";
+    var i = a.length - 1;
+    var j = b.length - 1;
+    var carry = 0;
+    while (i >= 0 || j >= 0 || carry > 0) {
+        carry += i >= 0 ? parseInt(a[i--]) : 0;
+        carry += j >= 0 ? parseInt(b[j--]) : 0;
+        result = carry % 2 + result;
+        carry = parseInt(carry / 2);
     }
-    let idx1 = a.length - 1;
-    let idx2 = b.length - 1;
-    let sum = "";
-    let carry = 0;
-    
-    while (idx1 >= 0) {
-        let n1 = parseInt(a[idx1]) , n2 = parseInt(b[idx2]);
-        let currDigitSum = carry + n1 + n2;
-        if (currDigitSum > 1) {
-            carry = 1;
-        } else {
-            carry = 0;
-        }
-        sum = (currDigitSum % 2) + sum;
-        idx1--;
-        idx2--;
-    }
-    
-    while (idx2 >= 0) {
-        let currDigitSum = carry + parseInt(b[idx2]);
-        if (currDigitSum > 1) {
-            carry = 1;
-        } else {
-            carry = 0;
-        }
-        sum = (currDigitSum % 2) + sum;
-        idx2--;
-    }
-    
-    return carry > 0 ? carry + sum : sum;
+    return result;
 };
