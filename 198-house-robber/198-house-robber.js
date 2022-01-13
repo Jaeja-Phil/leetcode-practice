00b01b78817f480716816c86568ef8d99dbcdb4c
Wonjun (Phillip) Choi <jaeja.phil@gmail.com>
2022-01-13 22:43:55 +0900

    Time: 89 ms (24.72%), Space: 38.6 MB (74.78%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -2,12 +2,15 @@ var rob = function(nums) {
     if (nums.length === 0) return 0;
     if (nums.length <= 2) return Math.max(...nums);
 
-    const dp = [nums[0], nums[1]];
-    
+    let maxTwoBefore = nums[0];
+    let maxOneBefore = Math.max(nums[0], nums[1]);
+
     for (let i = 2; i < nums.length; i++) {
-        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
-        dp[i - 1] = Math.max(dp[i - 1], dp[i - 2]);
+        const currMax = Math.max(nums[i] + maxTwoBefore, maxOneBefore);
+
+        maxTwoBefore = maxOneBefore;
+        maxOneBefore = currMax;
     }
 
-    return dp[dp.length - 1];
+    return maxOneBefore;
 }
