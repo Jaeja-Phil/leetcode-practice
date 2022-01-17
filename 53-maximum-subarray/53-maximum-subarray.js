543242e50e8adfa9f47c2d85857b47b5dd67e9fe
Wonjun (Phillip) Choi <jaeja.phil@gmail.com>
2022-01-17 20:40:24 +0900

    Time: 114 ms (41.90%), Space: 48.3 MB (92.30%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -1,20 +1,10 @@
-var maxSubArray = function(nums) {
-    let totalMax = nums[0];
-    let currSum = nums[0];
+const maxSubArray = function(nums) {
+    let prev = nums[0];
+    let max = nums[0];
 
-    for (let i = 1; i < nums.length; i++) {
-        const currNum = nums[i];
-        currSum += currNum;
-        // if contiguous sum of array is positive, it is worth keeping
-        if (currSum < 0) {
-            currSum = currNum;
-        } else {
-            // if contiguous sum of array becomes negative, abandon it, make current number current max
-            currSum = Math.max(currSum, currNum);
-        }
-        
-        totalMax = Math.max(totalMax, currSum);
-    }
-
-    return totalMax;
+    for(let i = 1; i < nums.length; i++){
+        prev = Math.max(nums[i], prev + nums[i]);
+        max = Math.max(prev, max);
+    } 
+    return max;
 };
